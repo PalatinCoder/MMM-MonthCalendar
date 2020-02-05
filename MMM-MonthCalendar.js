9a1a84c19b5f17e7502485b2a2ceb9ef03d8d107
Jan Syring-Lingenfelder <github@jan-sl.de>
2020-02-05 21:36:53 +0100

    feat: automatic update

--- MMM-MonthCalendar.js
+++ MMM-MonthCalendar.js
@@ -1,8 +1,27 @@
 Module.register("MMM-MonthCalendar", {
 
+    defaults: {
+        updateDelay: 5
+    },
+
     weekdays: [],
     start: function() {
         this.weekdays = [0,1,2,3,4,5,6].map(d => moment().weekday(d).format("ddd"));
+        this.scheduleUpdate();
+    },
+
+    update: function() {
+        this.updateDom(0);
+        this.scheduleUpdate();
+    },
+
+    scheduleUpdate: function() {
+        let nextUpdate = moment().startOf('day').add({ days: 1, seconds: this.config.updateDelay});
+        let timeout = nextUpdate.diff(moment());
+        setTimeout(() => {
+           this.update();
+        }, timeout);
+        Log.info(`${this.name} Next update scheduled at ${nextUpdate} which is in exactly ${timeout}ms`);
     },
 
     getHeader: function() {
