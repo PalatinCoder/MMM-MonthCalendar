5cda8eee14e08c9038d42eb39dab9e3aff45cf2c
Jan Syring-Lingenfelder <github@jan-sl.de>
2020-02-05 21:07:02 +0100

    feat: show calendar grid

--- MMM-MonthCalendar.js
+++ MMM-MonthCalendar.js
@@ -9,14 +9,34 @@ Module.register("MMM-MonthCalendar", {
         return `${this.data.header} ${moment().format('MMMM YYYY')}`
     },
 
+    getStyles: function() {
+        return [
+            `${this.data.path}/main.css`
+        ]
+    },
+
     getTemplate: function() {
         return `${this.name}.njk`;
     },
 
     getTemplateData: function() {
         return {
-            weekdays: this.weekdays
+            weekdays: this.weekdays,
+            days: this.getCalendarDays()
         }
+    },
+
+    getCalendarDays: function() {
+        let days = [];
+        let FillBefore = moment().startOf('month').weekday();
+        for (let i = 0; i < FillBefore; i++) {
+            days.push('');
+        }
+        for (let day = 1; day <= moment().daysInMonth(); day++) {
+            days.push(day);
+        }
+        Log.info(days);
+        return days;
     }
 
 });
